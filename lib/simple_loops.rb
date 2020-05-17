f7a940bb25459a18935f738365ad5176bebb386f
Esther Kang <estherkang14@gmail.com>
2020-05-17 20:51:26 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,9 +1,9 @@
 # Write your methods here
 
-#def loop_message_fives_times(array)
+#def loop_message_fives_times(message)
 #end
 
-#def loop_message_n_times(array)
+#def loop_message_n_times(message, number)
 #end
 
 #def output_array(array)
