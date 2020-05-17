a00bfc3e5a984bd616fa9240cb7510df0551a1b1
Esther Kang <estherkang14@gmail.com>
2020-05-17 20:59:27 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -11,8 +11,16 @@ end
 
 loop_message_five_times("Hello World.")
 
-#def loop_message_n_times(message, number)
-#end
+def loop_message_n_times(message, number)
+  counter = 0 
+  
+  while counter <= number do
+    puts message
+    counter += 1 
+end
+
+loop_message_n_times("Hello Moon.", 5)
+
 
 #def output_array(array)
 #end
