0a3349f23900e3e62a142934b852749d18013239
Esther Kang <estherkang14@gmail.com>
2020-05-17 20:55:10 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,7 +1,14 @@
 # Write your methods here
 
 def loop_message_fives_times(message)
-#end
+  counter = 0 
+  message = "My name is Esther"
+  
+  while counter <= 5 do 
+    puts message
+    counter += 1 
+  end
+end
 
 #def loop_message_n_times(message, number)
 #end
